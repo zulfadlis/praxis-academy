0fb80331741d703d153723c5c16f6826bef1c3d4
Muhammad Zulfadli <zulfadlisimatupang48@gmail.com>
2020-08-19 15:35:24 +0700

    m01 d02

--- novice/01-02/kasus/06.js
+++ novice/01-02/kasus/06.js
@@ -7,18 +7,18 @@ for (let i in siswa) {
 let jumlah = siswa.length;
 let avg = total / jumlah;
 
-console.log(avg);
+console.log(`Nilai rata  : ${avg}`);
 
 if (avg <= 60) {
-  console.log("F");
+  console.log("Grade : F");
 } else if (avg <= 70) {
-  console.log("D");
+  console.log("Grade : D");
 } else if (avg <= 80) {
-  console.log("C");
+  console.log("Grade : C");
 } else if (avg <= 90) {
-  console.log("B");
+  console.log("Grade : B");
 } else if (avg <= 100) {
-  console.log("A");
+  console.log("Grade: A");
 } else {
   console.log("tidak dapat ditemukan");
 }
